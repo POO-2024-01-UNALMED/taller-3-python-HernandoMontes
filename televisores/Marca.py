3bcf90367e4e45e79358b61fc7e6728a1b2cb4ee
HernandoMontes <hmontesg@unal.edu.co>
2024-02-26 19:15:55 -0500

    intento3

--- televisores/Marca.py
+++ televisores/Marca.py
@@ -2,10 +2,8 @@ class Marca:
     def __init__(self, nombre) -> None:
         self.__nombre=nombre
 
-
     def setNombre(self,nombre) -> None:
         self.__nombre=nombre
-
 
     def getNombre(self) -> str:
         return self.__nombre
--- televisores/TV.py
+++ televisores/TV.py
@@ -1,7 +1,6 @@
 class TV:
     numTV=0
 
-
     def __init__(self, marca, estado) -> None:
         self.__marca=marca
         self.__estado=estado
@@ -10,7 +9,6 @@ class TV:
         self.__precio=500
         TV.numTV+=1
 
-
     @classmethod
     def setNumTV(cls, num) -> None:
         cls.numTV=num
@@ -18,12 +16,10 @@ class TV:
     def getNumTV(cls) -> int:
         return cls.numTV
 
-
     def setMarca(self, marca) -> None:
         self.__marca=marca
     def getMarca(self):
         return self.__marca
-
 
     def setCanal(self, canal) -> None :
         if  self.__estado == True and canal>=1 and canal<=120:
@@ -37,12 +33,10 @@ class TV:
         if self.__estado==True and self.__canal>1 and self.__canal<=120:
             self.__canal-=1
 
-
     def setPrecio(self, precio) -> None:
         self.__precio=precio
     def getPrecio(self) -> int:
         return self.__precio
-
 
     def setVolumen(self,volumen) -> None:
         if self.__estado==True and volumen>=0 and volumen<=7:
@@ -56,12 +50,10 @@ class TV:
         if self.__estado==True and self.__volumen>0 and self.__volumen<=7:
             self.__volumen-=1
 
-
     def setControl(self, control) -> None:
         self.__control=control
     def getControl(self):
         return self.__control
-
 
     def turnOn(self) -> None:
         self.__estado=True
